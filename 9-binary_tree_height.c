1db225de7b4c17cf4de0319d1d0839c1a0eedb76
Munjogu123 <fredmunjogu@gmail.com>
2024-03-27 02:51:40 +0300

    measures the height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,20 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * max - returns the largest number
- * @a: operand 1
- * @b: operand 2
- *
- * Return: largest number
-*/
-size_t max(size_t a, size_t b)
-{
-	if (a > b)
-		return (a);
-	return (b);
-}
-
-/**
  * binary_tree_height - measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height
  *
